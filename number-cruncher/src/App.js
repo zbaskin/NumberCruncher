08e46801aa910646e81cfccdb7e2b960ba031f3b
Zach Baskin <zbaskin@gatech.edu>
2023-02-03 15:46:12 -0500

    fix loading results bug

--- number-cruncher/src/App.js
+++ number-cruncher/src/App.js
@@ -5,24 +5,22 @@ import Results from './Results.js';
 import callFunction from './functions.js';
 
 function App() {
-  const [combos, setCombos] = useState(0);
-  const [comboRange, setComboRange] = useState(0);
-  const [range, setRange] = useState(0);
-  const [hasMoreResults, setMoreResults] = useState(0);
+  const [combos, setCombos] = useState([]);
+  const [comboRange, setComboRange] = useState([]);
+  const [range, setRange] = useState(10);
+  const [hasMoreResults, setMoreResults] = useState(false);
 
   const getCombos = () => {
     var combinations = callFunction();
     setCombos(combinations);
-    setRange(10);
     setComboRange(combinations.slice(0, range));
-    if (combinations.length > 10) setMoreResults(true); 
+    if (combinations.length > range) setMoreResults(true); 
     else setMoreResults(false);
-    console.log(combos);
   }
 
   const getMoreResults = () => {
     setRange(range + 10);
-    setComboRange(combos.slice(0, range));
+    setComboRange(combos.slice(0, range + 10));
     if (combos.length > range) setMoreResults(true); 
     else setMoreResults(false);
   }
